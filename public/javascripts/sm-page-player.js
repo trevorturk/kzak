41f8d6117252caaccde6663b8fa4895630abaa26
Trevor Turk <trevorturk@gmail.com>
2009-10-29 22:39:20 -0500

    dont update page title

--- public/javascripts/sm-page-player.js
+++ public/javascripts/sm-page-player.js
@@ -31,11 +31,11 @@ function PagePlayer(oConfigOverride) {
     useWaveformData: false, // [Flash 9 only]: enable sound spectrum (raw waveform data) - WARNING: CPU-INTENSIVE: may set CPUs on fire.
     useEQData: false,       // [Flash 9 only]: enable sound EQ (frequency spectrum data) - WARNING: Also CPU-intensive.
     fillGraph: false,       // [Flash 9 only]: draw full lines instead of only top (peak) spectrum points
-    allowRightClick:true,   // let users right-click MP3 links ("save as...", etc.) or discourage (can't prevent.)
+    allowRightClick: true,   // let users right-click MP3 links ("save as...", etc.) or discourage (can't prevent.)
     useThrottling: false,   // try to rate-limit potentially-expensive calls (eg. dragging position around)
     autoStart: false,       // begin playing first sound when page loads
     playNext: true,         // stop after one sound, or play through list until end
-    updatePageTitle: true,  // change the page title while playing sounds
+    updatePageTitle: false,  // change the page title while playing sounds
     emptyTime: '-:--',      // null/undefined timer values (before data is available)
     useFavIcon: false       // try to show peakData in address bar (Firefox + Opera)
   }
