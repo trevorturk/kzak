7122b2d07a583288f2453fc68cf581b5c2655224
Trevor Turk <trevorturk@gmail.com>
2009-10-30 14:24:43 -0500

    move /uploads->/posts, enable cloudfront

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,16 +6,12 @@ class Post < ActiveRecord::Base
   attr_accessor :attachment_url  
   
   if CONFIG['s3']
-    has_attached_file :attachment, 
-                      :storage => :s3, 
-                      :path => ":filename", 
-                      :bucket => CONFIG['s3_bucket_name'],
+    has_attached_file :attachment, :storage => :s3, :path => ":filename", :bucket => CONFIG['s3_bucket_name'],
+                      :s3_host_alias => CONFIG['s3_host_alias'], :url => CONFIG['s3_host_alias'] ? ':s3_alias_url' : nil,
                       :s3_credentials => { :access_key_id => CONFIG['s3_access_id'], :secret_access_key => CONFIG['s3_secret_key'] },
                       :s3_headers => { 'Cache-Control' => 'max-age=315576000', 'Expires' => 10.years.from_now.httpdate }
   else
-    has_attached_file :attachment, 
-                      :storage => :filesystem, 
-                      :url => "/uploads/:filename"
+    has_attached_file :attachment, :storage => :filesystem, :url => "/posts/:filename"
   end
   
   belongs_to :user, :counter_cache => :posts_count
