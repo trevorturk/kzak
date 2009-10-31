9051da72a8a718caf3920e359567dcd7394f1726
Trevor Turk <trevorturk@gmail.com>
2009-10-31 17:42:12 -0500

    disable...?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
   
   def new_post_path_for_swf_upload
-    session_key = ActionController::Base.session_options[:key]
-    posts_path(session_key => cookies[session_key], request_forgery_protection_token => form_authenticity_token)
+    posts_path
+    # session_key = ActionController::Base.session_options[:key]
+    # posts_path(session_key => cookies[session_key], request_forgery_protection_token => form_authenticity_token)
   end
   
 end
