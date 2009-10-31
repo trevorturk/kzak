17f5e2c159440c53b5acbb154d93b67deb919072
Trevor Turk <trevorturk@gmail.com>
2009-10-31 17:40:53 -0500

    disable form auth token stuff for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   helper :all
-  protect_from_forgery
+  # protect_from_forgery # TODO figure out why this isn't working
   filter_parameter_logging :password, :password_confirmation
   
   def current_user
