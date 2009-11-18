dd8de8986af3e0ff6035ac7f9243b1a56049df53
Trevor Turk <trevorturk@gmail.com>
2009-11-18 17:53:05 -0600

    wip warden/devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   helper :all
   # protect_from_forgery # TODO figure out why this isn't working
-  filter_parameter_logging :password, :password_confirmation
-    
-  def require_login
-    redirect_to bullshit and return false unless user_signed_in?
-  end
-  
+  filter_parameter_logging :password, :password_confirmation  
 end
