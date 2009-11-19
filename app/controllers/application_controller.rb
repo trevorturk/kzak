b844ce93dd94ad9783d36d473f47fc988f3a8b19
Trevor Turk <trevorturk@gmail.com>
2009-11-18 18:24:19 -0600

    960gs for stylesheet grid, basic styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   helper :all
   # protect_from_forgery # TODO figure out why this isn't working
-  filter_parameter_logging :password, :password_confirmation  
+  filter_parameter_logging :password, :password_confirmation
+    
+  def current_user
+    @current_user ||= User.first
+  end
+  
 end
