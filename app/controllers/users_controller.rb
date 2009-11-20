f3d2ba2f0902da2c90eeddd589db8b6a4c24780e
Trevor Turk <trevorturk@gmail.com>
2009-11-20 15:58:38 -0800

    fix up users/show, dont show users feed, playable() playlist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
     
   def show
     @user = User.find_by_login! params[:id], :include => [:posts, :followings, :followers]
-    @feed_items = @user.feed_items.all :include => [:post, :poster]
   end
   
 end
