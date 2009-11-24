ccb2d90fc4828d3e67fea438ef187960d8772974
Trevor Turk <trevorturk@gmail.com>
2009-11-23 22:31:28 -0800

    clean up env.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,13 +6,13 @@ Rails::Initializer.run do |config|
   require 'yaml'
   CONFIG = (YAML.load_file('config/config.yml')[RAILS_ENV] rescue {}).merge(ENV)
   CONFIG['s3'] = true if CONFIG['s3_access_id'] && CONFIG['s3_secret_key'] && CONFIG['s3_bucket_name']  
-  config.frameworks -= [:active_resource, :action_mailer ]
+  config.frameworks -= [:active_resource, :action_mailer]
   config.time_zone = 'UTC'
-  config.gem 'thoughtbot-paperclip', :version => '2.3.0', :lib => 'paperclip', :source => 'http://gems.github.com'
+  config.gem 'thoughtbot-paperclip', :lib => 'paperclip', :version => '2.3.0'
   config.gem 'right_aws', :version => '1.9.0'
   config.gem 'right_http_connection', :version => '1.2.4'
-  config.gem 'ruby-mp3info', :lib => 'mp3info'
-  config.gem 'mime-types', :lib => 'mime/types'
+  config.gem 'ruby-mp3info', :lib => 'mp3info', :version => '0.6.13'
+  config.gem 'mime-types', :lib => 'mime/types', :version => '1.16'
   config.gem 'warden', :version => '0.6.4'
   config.gem 'devise', :version => '0.5.5'
   config.action_controller.session = {
