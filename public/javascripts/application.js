0694febed5d912b5934b2651a43fee3251ad1d71
Trevor Turk <trevorturk@gmail.com>
2009-11-27 07:57:56 -0600

    playable working

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,3 +3,40 @@ function number_to_human_size(bytes) {
   var e = Math.floor(Math.log(bytes)/Math.log(1024));
   return (bytes/Math.pow(1024, Math.floor(e))).toFixed(2)+" "+s[e];
 }
+
+$(document).ready(function(){
+  var sm = soundManager;
+
+  sm.url = '/flash';
+  sm.debugMode = false;
+  sm.useConsole = true;
+
+  sm.onready(function() {
+    $('a[href$=.mp3]').each(function() {
+      $(this).makePlayable();
+    });
+  });
+
+  $.fn.makePlayable = function() {
+    $(this).addClass('playable');
+    sm.createSound($(this)[0].id, $(this)[0].href);
+
+    $(this).click(function(e) {
+      e.preventDefault();
+      $(this).togglePlayState();
+    });
+  }
+
+  $.fn.togglePlayState = function() {
+    if ($(this).hasClass('playing')) {
+      $(this).removeClass('playing');
+      $(this).addClass('paused');
+      sm.pause($(this)[0].id);
+    } else {
+      $(this).removeClass('paused');
+      $(this).addClass('playing');
+      sm.play($(this)[0].id);
+    }
+  }
+});
+
