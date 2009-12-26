c74847bb1c53414d8b6fc414adcdd2aaabfd28b0
Trevor Turk <trevorturk@gmail.com>
2009-12-26 11:33:08 -0600

    style tweaks

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,4 @@
-function number_to_human_size(bytes) {
+function human_size(bytes) {
   var s = ['bytes', 'kb', 'MB', 'GB', 'TB', 'PB'];
   var e = Math.floor(Math.log(bytes)/Math.log(1024));
   return (bytes/Math.pow(1024, Math.floor(e))).toFixed(2)+" "+s[e];
