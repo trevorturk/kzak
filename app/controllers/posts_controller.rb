516ad05ef8a042fc6e2dfb8fb733c60804d4dc62
Trevor Turk <trevorturk@gmail.com>
2010-01-02 22:57:34 -0600

    password filters password_confirmation too

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  
+  skip_filter :request_forgery_protection, :only => :create
     
   before_filter :authenticate_user!, :only => :create
   before_filter :get_mime_type, :get_mp3_info, :only => :create
