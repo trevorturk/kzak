be0d920e4cf19375d0e4753527825dfcbf82695b
Trevor Turk <trevorturk@gmail.com>
2010-01-10 23:00:54 -0600

    mp3s marked unknown if not known

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,16 @@
 class PostsController < ApplicationController
-  
+
   skip_filter :request_forgery_protection, :only => :create # work around occasion invalid auth token in forms
-  
+
   before_filter :get_mime_type, :get_mp3_info, :only => :create
-  
+
   def index
     @posts = Post.all :include => :user
     @users = User.all
   end
-    
+
   def create
-    @post = current_user.posts.new :attachment => params[:Filedata], 
+    @post = current_user.posts.new :attachment => params[:Filedata],
                                    :title => @title, :artist => @artist, :album => @album
     if @post.save
       render :partial => @post
@@ -18,25 +18,25 @@ class PostsController < ApplicationController
      render :action => "new"
     end
   end
-  
+
   # def destroy
   #   @post = current_user.posts.find(params[:id])
   #   @post.destroy
   #   redirect_to root_path
   # end
-  
+
   protected
-  
+
   def get_mime_type
     params[:Filedata].content_type = MIME::Types.type_for(params[:Filedata].original_filename).to_s
   end
-  
+
   def get_mp3_info
     Mp3Info.open(params[:Filedata].path) do |r|
-      @title = r.tag.title
-      @artist = r.tag.artist
-      @album = r.tag.album
+      @title = r.tag.title || 'Unknown'
+      @artist = r.tag.artist || 'Unknown'
+      @album = r.tag.album || 'Unknown'
     end
   end
-  
+
 end
