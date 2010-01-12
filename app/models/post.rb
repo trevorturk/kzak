e263a0c776d3f7bd47a8adc51cbe10557a83231a
Trevor Turk <trevorturk@gmail.com>
2010-01-11 20:25:45 -0600

    privatize s3 uploads, drop posts/ prefix, use git length rand string for file name

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,12 +6,12 @@ class Post < ActiveRecord::Base
   attr_accessor :attachment_url
 
   if CONFIG['s3']
-    has_attached_file :attachment, :storage => :s3, :path => "posts/:filename", :bucket => CONFIG['s3_bucket_name'],
+    has_attached_file :attachment, :storage => :s3, :path => ":filename", :bucket => CONFIG['s3_bucket_name'],
                       :s3_host_alias => CONFIG['s3_host_alias'], :url => CONFIG['s3_host_alias'] ? ':s3_alias_url' : nil,
                       :s3_credentials => { :access_key_id => CONFIG['s3_access_id'], :secret_access_key => CONFIG['s3_secret_key'] },
                       :s3_permissions => 'private', :s3_headers => { 'Cache-Control' => 'max-age=315576000', 'Expires' => 10.years.from_now.httpdate }
   else
-    has_attached_file :attachment, :storage => :filesystem, :url => "/system/posts/:filename"
+    has_attached_file :attachment, :storage => :filesystem, :url => "/system/:filename"
   end
 
   belongs_to :user, :counter_cache => :posts_count
@@ -44,7 +44,7 @@ class Post < ActiveRecord::Base
 
   def randomize_file_name
     extension = File.extname(attachment_file_name).downcase
-    self.attachment.instance_write(:file_name, "#{ActiveSupport::SecureRandom.hex(16)}#{extension}")
+    self.attachment.instance_write(:file_name, "#{ActiveSupport::SecureRandom.hex(20)}#{extension}")
   end
 
   def attachment_url_provided?
