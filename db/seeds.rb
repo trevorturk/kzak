0d3eceae18c39976cf605fa686d19f095b262867
Trevor Turk <trevorturk@gmail.com>
2010-01-11 20:39:55 -0600

    rake db:seed to create admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+pass = ActiveSupport::SecureRandom.hex(20)
+User.create!(:login => 'admin', :email => CONFIG['mailer'], :password => pass, :password_confirmation => pass)
+puts "...created 'admin' user with password '#{pass}'"
