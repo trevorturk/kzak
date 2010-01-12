1fbcaefaccd7d520a8dd378267c397eb6d5d2e06
Trevor Turk <trevorturk@gmail.com>
2010-01-11 20:17:11 -0600

    wip use private bucket

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,66 +1,66 @@
 class Post < ActiveRecord::Base
   require 'mp3info'
   require 'open-uri'
-  
+
   attr_accessible :attachment, :attachment_url, :attachment_remote_url, :title, :artist, :album
-  attr_accessor :attachment_url  
-  
+  attr_accessor :attachment_url
+
   if CONFIG['s3']
     has_attached_file :attachment, :storage => :s3, :path => "posts/:filename", :bucket => CONFIG['s3_bucket_name'],
                       :s3_host_alias => CONFIG['s3_host_alias'], :url => CONFIG['s3_host_alias'] ? ':s3_alias_url' : nil,
                       :s3_credentials => { :access_key_id => CONFIG['s3_access_id'], :secret_access_key => CONFIG['s3_secret_key'] },
-                      :s3_headers => { 'Cache-Control' => 'max-age=315576000', 'Expires' => 10.years.from_now.httpdate }
+                      :s3_permissions => 'private', :s3_headers => { 'Cache-Control' => 'max-age=315576000', 'Expires' => 10.years.from_now.httpdate }
   else
     has_attached_file :attachment, :storage => :filesystem, :url => "/system/posts/:filename"
   end
-  
+
   belongs_to :user, :counter_cache => :posts_count
-  
+
   before_validation :download_remote_file, :if => :attachment_url_provided?
-    
+
   validates_presence_of :user_id, :attachment_file_name
   validates_presence_of :attachment_remote_url, :if => :attachment_url_provided?, :message => 'is invalid or inaccessible'
   validates_attachment_content_type :attachment, :content_type => ['application/mp3','application/x-mp3','audio/mpeg','audio/mp3']
-    
+
   before_create :randomize_file_name
   after_create :create_feed_items
   after_destroy :destroy_feed_items
-  
+
   default_scope :order => 'created_at DESC'
-    
+
   def create_feed_items
     FeedItem.populate(self)
   end
-  
+
   def destroy_feed_items
     FeedItem.unpopulate(self)
   end
-  
+
   def to_s
     attachment_file_name
   end
-  
+
   protected
-    
+
   def randomize_file_name
     extension = File.extname(attachment_file_name).downcase
     self.attachment.instance_write(:file_name, "#{ActiveSupport::SecureRandom.hex(16)}#{extension}")
   end
-  
+
   def attachment_url_provided?
     !self.attachment_url.blank?
   end
-  
+
   def download_remote_file
     self.attachment = do_download_remote_file
     self.attachment_remote_url = attachment_url
   end
-  
+
   def do_download_remote_file
     io = open(URI.parse(attachment_url))
     def io.original_filename; base_uri.path.split('/').last; end
     io.original_filename.blank? ? nil : io
   rescue # catch url errors with validations instead of exceptions (Errno::ENOENT, OpenURI::HTTPError, etc...)
   end
-  
+
 end
--- lib/tasks/s3.rake
+++ lib/tasks/s3.rake
@@ -3,7 +3,7 @@ namespace :s3 do
     puts "Reading config/config.yml and creating a bucket on s3 for the production environment..."
     CONFIG = YAML.load_file('config/config.yml')['production'] rescue {}
     s3 = RightAws::S3.new(CONFIG['s3_access_id'], CONFIG['s3_secret_key'])
-    RightAws::S3::Bucket.create(s3, CONFIG['s3_bucket_name'], true)
+    RightAws::S3::Bucket.create(s3, CONFIG['s3_bucket_name'], true, 'private')
     s3.bucket(CONFIG['s3_bucket_name'])
     puts "OK"
   end
