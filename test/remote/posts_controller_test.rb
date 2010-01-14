6b23759b4b0615256365f21a301919010b2fc1ce
Trevor Turk <trevorturk@gmail.com>
2010-01-13 21:22:58 -0600

    small test cleanup

--- test/remote/posts_controller_test.rb
+++ test/remote/posts_controller_test.rb
@@ -1,23 +1,9 @@
-# require File.dirname(__FILE__) + '/../test_helper'
-# 
-# class PostsControllerTest < ActionController::TestCase
-#   
-#   # ===
-#   # NOTE these are real remote requests that can be run with "rake test:remote" - they won't be run with autotest
-#   # ===
-#     
-#   test "should create post via (real) url" do
-#     assert_difference 'Post.count' do
-#       post :create, :post => { :attachment_url => 'http://www.google.com/intl/en_ALL/images/logo.gif' }
-#     end
-#     assert_redirected_to root_path
-#   end
-#   
-#   test "should not bomb on post via bogus (real) url" do
-#     assert_no_difference 'Post.count' do
-#       post :create, :post => { :attachment_url => 'invalid' }
-#     end
-#     assert_response :success
-#   end
-#       
-# end
+require File.dirname(__FILE__) + '/../test_helper'
+
+class PostsControllerTest < ActionController::TestCase
+
+  # ===
+  # NOTE these are real remote requests that can be run with "rake test:remote" - they won't be run with autotest
+  # ===
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,17 +4,16 @@ require File.expand_path(File.dirname(__FILE__) + "/blueprints")
 require 'test_help'
 
 class ActiveSupport::TestCase
-  
+
   setup do
     Sham.reset
   end
-  
+
   def sign_in!(options = {})
     user = User.make(options)
     sign_in user
     user
   end
-     
 end
 
 class ActionController::TestCase
