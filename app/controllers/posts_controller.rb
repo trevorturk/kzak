f3b9a3e9f7ae1f49dc979b52fd53649829c25038
Trevor Turk <trevorturk@gmail.com>
2010-12-01 01:54:58 +0000

    attempt to fix file uploads for rails 3.0.3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,24 +29,30 @@ class PostsController < ApplicationController
   protected
 
   def get_filename
-    @filename ||= params[:Filedata].original_filename rescue nil # for posts/new form
+    @filename ||= params[:Filedata].original_filename if params[:Filedata]
   end
 
   def get_audio_info
+    return unless params[:Filedata]
+
+    path = params[:Filedata].try(:path) || params[:Filedata].tempfile.path
+
     if @filename =~ /.mp3$/
-      Mp3Info.open(params[:Filedata].path) do |r|
+      Mp3Info.open(path) do |r|
         @title = r.tag.title
         @artist = r.tag.artist
         @album = r.tag.album
       end
     elsif @filename =~ /.mp4$|.m4a$/
-      info = MP4Info.open(params[:Filedata].path)
+      info = MP4Info.open(path)
       @title = info.send(:NAM)
       @artist = info.send(:ART)
       @album = info.send(:ALB)
     end
+
     @title = @title.toutf8 rescue 'Unknown'
     @artist = @artist.toutf8 rescue 'Unknown'
     @album = @album.toutf8 rescue 'Unknown'
   end
+
 end
