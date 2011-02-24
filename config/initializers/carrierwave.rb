38b0f0f55dfa3ca26719fa4f35fd23a2e16194b3
Trevor Turk <trevorturk@gmail.com>
2011-02-24 10:38:13 +0000

    init

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,11 +11,8 @@ if CONFIG['s3']
     config.fog_attributes = {'Cache-Control' => 'max-age=315576000', 'Expires' => 99.years.from_now.httpdate}
     config.cache_dir = "#{Rails.root}/tmp/uploads"
   end
-end
-
-if Rails.env.test?
+else
   CarrierWave.configure do |config|
     config.storage = :file
-    config.enable_processing = false
   end
 end
